8d2824ed2260cac37269edbfbb19f09dcd3afd9d
YriyProtcyshin <yriy.protcyshin@gmail.com>
2023-04-20 19:22:17 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/yriyprotcyshin.github.io">
+    <BrowserRouter basename="/outletsTodos-frontEnd">
       <App />
     </BrowserRouter>
   </React.StrictMode>
