52068e24e1c224a692fd88ed8101d185d5e5ec10
YriyProtcyshin <yriy.protcyshin@gmail.com>
2023-04-25 16:57:36 +0300

    commit

--- src/components/table/TaskTable.jsx
+++ src/components/table/TaskTable.jsx
@@ -3,17 +3,18 @@ import { DataGrid } from '@mui/x-data-grid';
 import { Container } from '../Container/Container';
 
 const columns = [
-  { field: 'Customer_Code', headerName: 'Customer Code', width: 150 },
-  { field: 'Account_Name', headerName: 'Account Name', width: 130 },
+  { field: 'Customer_Code', headerName: 'Customer Code', width: 100 },
+  { field: 'Account_Name', headerName: 'Account Name', width: 170 },
+  { field: 'Address_Line_1', headerName: 'Address', width: 360 },
   { field: 'Commercial_(Legal)_Name', headerName: 'Legal Name', width: 130 },
-  { field: 'Ownership_Type', width: 90 },
+
   {
     field: 'Parent_Account_Name',
     headerName: 'Parent Account Name',
     width: 160,
   },
   { field: 'Key_Account_Type', headerName: 'Key Account Type', width: 160 },
-  { field: 'Address_Line_1', headerName: 'Address', width: 360 },
+
   { field: 'Status', headerName: 'Status' },
 ];
 
@@ -24,7 +25,7 @@ export default function DataTable({
 }) {
   return (
     <Container>
-      <div style={{ height: '450px', width: '100%' }}>
+      <div style={{ height: 'calc(100vh - 250px)', width: '100%' }}>
         <DataGrid
           rows={rows}
           columns={columns}
